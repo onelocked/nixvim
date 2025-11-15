f3691339e7c2d6513c99acdfc26fdbfb97ce4caf
onelocked <180497738+onelocked@users.noreply.github.com>
2025-11-15 23:25:05 +0000

    Add tab to autocomplete

--- modules/blink-cmp/default.nix
+++ modules/blink-cmp/default.nix
@@ -19,6 +19,11 @@ in
             "fallback"
           ];
 
+          "<Tab>" = [
+            "accept"
+            "fallback"
+          ];
+
           "<c-l>" = [
             "snippet_forward"
             "fallback"
@@ -34,32 +39,6 @@ in
           "<C-d>" = [
             "scroll_documentation_down"
             "fallback"
-          ];
-          "<C-space>" = [
-            (
-              # lua
-              mkRaw ''
-                function(cmp)
-                  local ok,_ = pcall(require, "copilot")
-                    if ok then
-                      vim.g.copilot_no_tab_map = true
-                      vim.g.copilot_assume_mapped = true
-                      vim.g.copilot_tab_fallback = ""
-
-                      local suggestion = require("copilot.suggestion")
-                      if suggestion.is_visible() then
-                        suggestion.accept()
-                      else
-                        if cmp.snippet_active() then
-                        return cmp.select_and_accept()
-                        else
-                        return cmp.accept()
-                        end
-                      end
-                    end
-                  end
-              ''
-            )
           ];
         };
       };
